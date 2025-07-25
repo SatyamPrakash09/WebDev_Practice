6390975381c1f8d1058ea6e2322ce4ec692c87a8
SATYAM PRAKASH <satyamprakash996@gmail.com>
2025-07-25 20:39:00 +0530

    Some changes

--- INLINE ELEMENTS/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('submit').addEventListener('click', function () {
-        window.location.href = 'file:///C:/Users/hp/Desktop/SIGMA%20WEB%20DEV%27/EXPERIMENT/index.html';
-});
